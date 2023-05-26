844255036ac71f2177cb52d68c224a15b3d17927
Luca Bolognese <lucabol@microsoft.com>
2023-05-26 14:34:04 +0200

    Add DodStruct

---
+++ DodStruct.h
@@ -0,0 +1,17 @@
+#ifndef DODSTRUCT_H
+#define DODSTRUCT_H
+
+#include "Utils.h"
+
+#ifndef SIDXT
+#define SIDXT Size
+#endif
+
+#define Struct1(sn,N,t1,n1) t1 sn##n1[N]; SIDXT sn##Idx = 0;
+#define Struct2(sn,N,t1,n1,t2,n2) Struct1(sn,N,t1,n1) t2 sn##n2[N];
+#define Struct3(sn,N,t1,n1,t2,n2,t3,n3) Struct2(sn,N,t1,n1,t2,n2) t3 sn##n3[N];
+#define Struct4(sn,N,t1,n1,t2,n2,t3,n3,t4,n4) Struct3(sn,N,t1,n1,t2,n2,t3,n3) t4 sn##n4[N];
+#define Struct5(sn,N,t1,n1,t2,n2,t3,n3,t4,n4,t5,n5) Struct4(sn,N,t1,n1,t2,n2,t3,n3,t4,n4) t5 sn##n5[N];
+
+
+#endif
